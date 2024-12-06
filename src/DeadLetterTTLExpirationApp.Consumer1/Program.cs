632ee97eda6771f7c442dc631f8bf98771851df5
mostafa jafari <jafari.mostafa@bcmlogic.com>
2024-12-06 15:38:18 +0100

    DeadLetter TTL Expriation sample done.

---
+++ src/DeadLetterTTLExpirationApp.Consumer1/Program.cs
@@ -0,0 +1,70 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+class Consumer
+{
+    static void Main()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            // Declare Main Queue with Dead Letter Exchange configuration and TTL
+            var arguments = new Dictionary<string, object>
+            {
+                { "x-dead-letter-exchange", "dead_letter_exchange" },
+                { "x-dead-letter-routing-key", "dead_letter" },
+                { "x-message-ttl", 2000 }
+            };
+
+            channel.QueueDeclare(queue: "main_queue_ttl", durable: true, exclusive: false, autoDelete: false, arguments: arguments);
+
+            channel.QueueDeclare(queue: "dead_letter_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+           
+            MainConsumer(channel, consumer);
+
+            var deadLetterConsumer = new EventingBasicConsumer(channel);
+
+            DeadLetterConsumer(channel, deadLetterConsumer);
+
+            channel.BasicConsume(queue: "main_queue_ttl", autoAck: false, consumer: consumer);
+            channel.BasicConsume(queue: "dead_letter_queue", autoAck: false, consumer: deadLetterConsumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    private static void MainConsumer(IModel channel, EventingBasicConsumer consumer)
+    {
+        consumer.Received += (model, ea) =>
+        {
+            System.Threading.Thread.Sleep(10000);
+
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Received {0}", message);
+
+            // Process message normally
+            Console.WriteLine(" [x] Message processed: {0}", message);
+            channel.BasicAck(ea.DeliveryTag, false);
+        };
+    }
+
+    private static void DeadLetterConsumer(IModel channel, EventingBasicConsumer deadLetterConsumer)
+    {
+        deadLetterConsumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(" [x] Dead Letter Queue - Received {0}", message);
+
+            channel.BasicAck(ea.DeliveryTag, false);
+        };
+    }
+}
--- src/DeadLetterTTLExpirationAppExe.ps1
+++ src/DeadLetterTTLExpirationAppExe.ps1
@@ -2,12 +2,12 @@ $currentDir = (Get-Location).Path
 
 # Run the Consumer1 project in a new tab
 wt new-tab --title "Consumer1"  powershell.exe -Command "
-cd \"$currentDir\DoubleWritingApp.Consumer1\";
+cd \"$currentDir\DeadLetterTTLExpirationApp.Consumer1\";
 dotnet run" `;
 
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 1
 
 # Run the Producer project in a new tab
-wt new-tab  --title "Producer"   powershell.exe -Command "
-cd \"$currentDir\DoubleWritingApp.Producer\";
+wt new-tab --title "Producer" powershell.exe -Command "
+cd \"$currentDir\DeadLetterTTLExpirationApp.Producer\";
 dotnet run"
