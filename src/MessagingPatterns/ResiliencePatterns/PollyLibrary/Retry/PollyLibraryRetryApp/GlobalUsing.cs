a4f71cb7f831c9d8fc8e0c492cf64e71e26715a6
mostafa jafari <jafari.mostafa@bcmlogic.com>
2024-12-16 20:01:27 +0100

    PollyLibraryRetryApp test project Done

---
+++ src/MessagingPatterns/ResiliencePatterns/PollyLibrary/Retry/PollyLibraryRetryApp/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using RabbitMQ.Client;
+global using System;
+global using System.Text;
+global using Newtonsoft.Json;
+global using Polly;
---
+++ src/MessagingPatterns/ResiliencePatterns/PollyLibrary/Retry/PollyLibraryRetryApp/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Polly.Retry;
+using RabbitMQ.Client.Exceptions;
+using System.Net.Sockets;
+
+public class Program
+{
+    private static readonly RetryPolicy _RetryPolicy =
+         Policy
+         .Handle<BrokerUnreachableException>()
+         .Or<SocketException>()
+         .WaitAndRetry(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+           (exception, timeSpan, retryCount, context) =>
+           {
+               Console.WriteLine($"Retry {retryCount} " +
+                   $"encountered an error: {exception.Message}. " +
+                   $"Waiting {timeSpan} before next retry.");
+           });
+    public static void Main(string[] args)
+    {
+        _RetryPolicy.Execute(() =>
+        {
+            //Connecting to RabbitMq
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            Console.ReadLine();
+        });
+    }
+}
